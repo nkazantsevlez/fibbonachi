6a637b843097d3902b360e0b719c944bfce6338a
Nikita Kazantsev <nkazantsevlez@gmail.com>
2021-04-09 17:30:37 +0800

    small changes, return of the standard progress bar

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.myapplication1;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
@@ -17,22 +20,35 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public void Calculatebutton_click(View view)
     {
+
         //чтоб взять число с EditText
         EditText inputnumbr=findViewById(R.id.inputnumber);
         int number=Integer.parseInt(inputnumbr.getText().toString());
         //Чтоб вывеси результат в TextView
         TextView outputnumbr= (TextView) findViewById(R.id.OutputNumber);
-
-        //вычисляем фиббоначи
+        //переменные для вычисления Фибоначчи
         int f = 0, g = 1;
+        //чтоб работать с прогрессбаром
+        ProgressBar mProgressBar=(ProgressBar) findViewById(R.id.mprogressBar);
+        //устанавливаем минимальное и максимальное значения progressbar
+        mProgressBar.setMax(number);
+        mProgressBar.setMin(0);
+        //устанавливаем шаг
+        int counter=0;
 
-        for (int i = 1; number >= i; i++) {
-            System.out.print(f + " ");
+        //вычисляем число Фибоначчи
+        for (int i = 1; number>= i; i++)
+        {
             f = f + g;
             g = f - g;
+
             outputnumbr.setText(String.valueOf(f));
+            counter++;
+            mProgressBar.setProgress(counter);
+
         }
 
     }
